c87f4f13f72cdccf8511547c357944e34facacbd
Muhammad Mohsin <mohsincloudmail@gmail.com>
2025-04-30 02:52:00 +0500

    mq updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,15 +13,15 @@ $(document).ready(function () {
         slidesToShow: 4,
         slidesToScroll: 1,
         dots: false,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 0,
-        speed: 2500,
+        speed: 2000,
         arrows: false,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 4,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                     infinite: true,
                     dots: false,
@@ -30,7 +30,7 @@ $(document).ready(function () {
             {
                 breakpoint: 767,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                     infinite: false,
                 }
